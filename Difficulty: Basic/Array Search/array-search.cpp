40f80128b79b8660113ab9a3ece87190004fe900
Aamodit <81962193+Aamodit@users.noreply.github.com>
2024-09-17 16:00:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.cpp
+++ Difficulty: Basic/Array Search/array-search.cpp
@@ -9,15 +9,16 @@ class Solution {
     // Function to search x in arr
     // arr: input array
     // X: element to be searched for
-    int search(vector<int>& arr, int x) {
-         int n = arr.size();
-        for(int i = 0;i<n;i++)
-        {
-            if(arr[i] == x)
-            return i;
-        }
-        return -1;
+int search(vector<int>& arr, int x) {
         // Your code here
+        int n = arr.size();
+        for(int i=0; i<n; i++) {
+            if(arr[i] == x) {
+                return i;
+                break;
+        }
+    }
+    return -1;
     }
 };
 
