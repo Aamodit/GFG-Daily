f5e107ebf27db8f64a6e3cc1070c06fd6d0611bd
Aamodit <81962193+Aamodit@users.noreply.github.com>
2024-12-30 03:35:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    int findUnion(vector<int>& a, vector<int>& b) {
+        int n = a.size();
+        int m = b.size();
+        for(int i = 0;i<b.size();i++){
+            a.push_back(b[i]);
+        }
+        unordered_set<int> st;
+        for(int i=0;i<n+m;i++){
+            st.insert(a[i]);
+        }
+        return st.size();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after reading t
+
+    while (t--) {
+        vector<int> a;
+        vector<int> b;
+
+        string input;
+        // For a
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // For b
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findUnion(a, b) << endl;
+        cout << '~' << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
