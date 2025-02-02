b5d374506b41e071e0f8af345248f3e6f32042cf
Aamodit <81962193+Aamodit@users.noreply.github.com>
2025-02-03 00:04:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.cpp
@@ -0,0 +1,154 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after splitting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+// User function template for C++
+
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    // Function to find the height of a binary tree.
+    void levelOrderTraversal(Node *node, int &maxi)
+    {
+        queue<Node*> q1;
+        q1.push(node);
+        int level = 0;
+        
+        while(!q1.empty())
+        {
+            int sz = q1.size();
+            
+            for(int i=0;i<sz;i++)
+            {
+                Node *current = q1.front();
+                q1.pop();
+                
+                if(current->left==nullptr && current->right==nullptr)
+                {
+                    if(level>maxi)
+                    {
+                        maxi = level;
+                    }
+                }
+                
+                if(current->left!=nullptr)
+                    q1.push(current->left);
+                    
+                if(current->right!=nullptr)
+                    q1.push(current->right);
+            }
+            
+            level++;
+        }
+    }
+    
+    int height(Node* node) {
+        int maxi = INT_MIN;
+        levelOrderTraversal(node, maxi);
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string treeString;
+        getline(cin, treeString);
+        Node* root = buildTree(treeString);
+        Solution ob;
+        cout << ob.height(root) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
