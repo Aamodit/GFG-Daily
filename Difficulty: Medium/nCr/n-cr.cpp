5b6e1e2ee52d044df67728c938989190d01f8da2
Aamodit <81962193+Aamodit@users.noreply.github.com>
2025-05-13 01:42:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int nCr(int n, int r) {
+        // code here
+        if(r>n) return 0;
+        
+        long res = 1;
+
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return (int)res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, r;
+        cin >> n >> r;
+
+        Solution ob;
+        cout << ob.nCr(n, r) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
