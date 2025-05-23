7d69d06522a73a3a8072bf1287bafada76bf5868
Aamodit <81962193+Aamodit@users.noreply.github.com>
2025-05-24 00:46:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of all substrings of a number/sum-of-all-substrings-of-a-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long sumSubstrings(string s) {
+        int n = s.size();
+        long long prev = s[0] - '0';
+        long long ans = prev;
+
+        for (int i = 1; i < n; i++) {
+            long long curr = prev * 10 + (s[i] - '0') * (i + 1);
+            ans += curr;
+            prev = curr;
+        }
+
+        return ans;
+    }
+};
