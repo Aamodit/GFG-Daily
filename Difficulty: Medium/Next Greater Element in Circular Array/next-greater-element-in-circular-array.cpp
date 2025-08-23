e74e9852ce63b60b6f926320661eaf3a2ef51488
Aamodit <81962193+Aamodit@users.noreply.github.com>
2025-08-23 14:13:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int> &arr) {
+        // Get index of largest final element in the array
+        int idx = 0;
+        int n = arr.size();
+        
+        for(int i = 0;i<n;i++){
+            if(arr[i]>arr[idx]){
+                idx = i;
+            }
+        }
+        
+        vector<int> sol(arr.size(),-1);
+        stack<int> st;
+        
+        // We start iterating just after the largest element in the 
+        // circular fashion upto the largest element
+        
+        for(int i = idx+1;i<=idx+n;i++){
+            while((!st.empty()) and (arr[i%n]>arr[st.top()])){
+                sol[st.top()] = arr[i%n];
+                st.pop();
+            }
+            st.push(i%n);
+        }
+        
+        return sol;
+    }
+};
