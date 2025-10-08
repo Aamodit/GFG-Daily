40a990f8804b861738b5641f81b6283d6fc7afa6
Aamodit <81962193+Aamodit@users.noreply.github.com>
2025-10-09 01:05:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Postorder Traversal/postorder-traversal.cpp
@@ -0,0 +1,35 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void fun(Node* root, vector<int> &ans){
+        if(!root)
+          return;
+          
+        fun(root->left, ans);  
+        fun(root->right, ans);
+        
+        ans.push_back(root->data);
+        return;
+    }
+  
+    vector<int> postOrder(Node* root) {
+        // code here
+        vector<int> ans;
+        fun(root, ans);
+        return ans;
+    }
+};
