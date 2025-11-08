238ba4e8d64e4f4e5dad4abd3b063bd7b553a533
Aamodit <81962193+Aamodit@users.noreply.github.com>
2025-11-09 00:05:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Chocolate Pickup II/chocolate-pickup-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int n;
+    int dp[51][51][51];
+
+    int dfs(vector<vector<int>> &mat, int r1, int c1, int r2) {
+        int c2 = r1 + c1 - r2;
+        if (r1 >= n || c1 >= n || r2 >= n || c2 >= n)
+            return -1e9;
+        if (mat[r1][c1] == -1 || mat[r2][c2] == -1)
+            return -1e9;
+        if (r1 == n - 1 && c1 == n - 1)
+            return mat[r1][c1];
+        if (dp[r1][c1][r2] != -1)
+            return dp[r1][c1][r2];
+        int res = mat[r1][c1];
+        if (r1 != r2 || c1 != c2)
+            res += mat[r2][c2];
+        int temp = max({
+            dfs(mat, r1 + 1, c1, r2 + 1),
+            dfs(mat, r1, c1 + 1, r2),
+            dfs(mat, r1 + 1, c1, r2),
+            dfs(mat, r1, c1 + 1, r2 + 1)
+        });
+        res += temp;
+        return dp[r1][c1][r2] = res;
+    }
+
+    int chocolatePickup(vector<vector<int>> &mat) {
+        n = mat.size();
+        memset(dp, -1, sizeof(dp));
+        int ans = dfs(mat, 0, 0, 0);
+        return max(0, ans);
+    }
+};
