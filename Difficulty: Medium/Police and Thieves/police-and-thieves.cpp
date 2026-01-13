f525409d0b90e01e589b2f126d0617c8ac0f7cbc
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-01-14 01:53:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    virtual int catchThieves(vector<char> &arr, int k){
+      int i=0, j=0, n=arr.size();
+      int ans=0;
+      
+      while(i<n && j<n){
+        if(arr[j] == 'T') { j++; continue; }
+        if(arr[i] == 'P') { i++; continue; }
+        
+        if(arr[j]=='P' && arr[i]=='T'){
+          if(abs(i-j) <= k){
+            ans++; i++; j++;      
+          }
+          else if(i>j)
+            j++;
+          else
+            i++;
+        }
+        
+      }
+      
+      return ans;
+    }
+};
