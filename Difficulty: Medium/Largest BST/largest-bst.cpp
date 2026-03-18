d2e22eee0c66dc851651a143cce1a5f154f490fc
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-03-19 01:43:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest BST/largest-bst.cpp
@@ -0,0 +1,53 @@
+class Solution {
+  public:
+
+    class Info {
+      public:
+        int size;
+        int minVal;
+        int maxVal;
+        bool isBST;
+
+        Info(int s, int mn, int mx, bool b){
+            size = s;
+            minVal = mn;
+            maxVal = mx;
+            isBST = b;
+        }
+    };
+
+    int maxSize = 0;
+
+    Info solve(Node* root){
+
+        if(root == NULL){
+            return Info(0, INT_MAX, INT_MIN, true);
+        }
+
+        Info left = solve(root->left);
+        Info right = solve(root->right);
+
+        // check BST condition
+        if(left.isBST && right.isBST && 
+           root->data > left.maxVal && root->data < right.minVal){
+
+            int currSize = left.size + right.size + 1;
+
+            maxSize = max(maxSize, currSize);
+
+            return Info(
+                currSize,
+                min(root->data, left.minVal),
+                max(root->data, right.maxVal),
+                true
+            );
+        }
+
+        return Info(0, 0, 0, false);
+    }
+
+    int largestBst(Node *root) {
+        solve(root);
+        return maxSize;
+    }
+};
