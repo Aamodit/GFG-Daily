ca1145ea34ac75365f9de5f2e1abfe5f1d35fc43
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-04-04 03:17:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gray Code/gray-code.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    vector<string> graycode(int n) {
+        vector<string> ans;
+        
+        for(int mask=0;mask<(1<<n);mask++){
+            int g = mask ^ (mask >> 1);
+            
+            string s="";
+            for(int i=n-1;i>=0;i--){
+                if(g&(1<<i)) s+='1';
+                else s+='0';
+            }
+            
+            ans.push_back(s);
+        }
+        
+        return ans;
+    }
+};
+
