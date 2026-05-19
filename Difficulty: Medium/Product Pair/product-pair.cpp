26387426c51911c6fee5aae7fe402ec36aa4bd38
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-05-20 01:04:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Product Pair/product-pair.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool isProduct(vector<int>& arr, long long target) {
+        unordered_set<int> st;
+        for(auto it : arr)
+        {
+            if((it && target%abs(it)==0 && st.find(target/it)!=st.end()) || (!it && !target))
+            return true;
+            st.insert(it);
+        }
+        return false;
+    }
+};
