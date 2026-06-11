2d99fb60979deb723106a460e73a6661e0c770b7
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-06-12 00:32:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check Repeated Substring with K Replacements/check-repeated-substring-with-k-replacements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    bool kSubstr(string &s, int k) {
+        int n = s.size();
+
+        if (n % k != 0) return false;
+
+        unordered_map<string, int> freq;
+        int blocks = n / k;
+        int mx = 0;
+
+        for (int i = 0; i < n; i += k) {
+            string part = s.substr(i, k);
+            mx = max(mx, ++freq[part]);
+        }
+
+        return mx >= blocks - 1;
+    }
+};
