655ee1ea47ceca622d21428359d85010ad542686
Aamodit <81962193+Aamodit@users.noreply.github.com>
2026-09-04 00:19:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bird and Max Fruit Gathering/bird-and-max-fruit-gathering.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    virtual int maxFruits(vector<int> arr, int m) {
+      int ans=0, n=arr.size();
+
+      int temp=0;
+      for(int i=0;i<m;i++){
+          temp += arr[i];
+      }
+
+      ans = max(ans, temp);
+
+      for(int i=1;i<n;i++){
+          temp -= arr[i-1];
+          temp += arr[(i+m-1)%n];
+          ans = max(ans, temp);
+      }
+
+      return ans;
+
+    }
+};
+
